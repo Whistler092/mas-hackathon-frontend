8a9d5356a89814e72e671348dc873eb511499970
sirusalazar <sirusalazar@gmail.com>
2020-07-10 15:57:48 -0500

    added detector and receptor modules

---
+++ src/app/detector/detector.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecorderComponent } from './components/recorder/recorder.component';
+
+@NgModule({
+  declarations: [RecorderComponent],
+  imports: [CommonModule],
+})
+export class DetectorModule {}
